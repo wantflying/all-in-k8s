041a178c5d2346289e1e7329ad02db857ecc8228
wantflying <xiushengyuan@gmail.com>
2024-10-15 21:15:12 +0800

    fix： 暂时屏蔽k8s pod

--- natter-exportconfig/src/test/java/org/yyy/homek8s/natterexportconfig/NatterExportconfigApplicationTests.java
+++ natter-exportconfig/src/test/java/org/yyy/homek8s/natterexportconfig/NatterExportconfigApplicationTests.java
@@ -1,9 +1,6 @@
 package org.yyy.homek8s.natterexportconfig;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.yyy.homek8s.natterexportconfig.controller.PodLogMonitorController;
 
 @SpringBootTest
 class NatterExportconfigApplicationTests {
